0c6fc29474fcb93adbaa5093bfc820856be547ca
Amit <amit231d@gmail.com>
2025-12-17 12:35:01 +0530

    Cleanup: Remove development testing files for production release

--- test_sess_get.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_set_cookie_params(0, '/');
-session_start();
-echo "Session ID: " . session_id() . "<br>";
-echo "Test Var: " . ($_SESSION['test_var'] ?? 'NOT SET') . "<br>";
-echo "Time: " . ($_SESSION['time'] ?? 'NOT SET');
-?>
--- test_sess_set.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_set_cookie_params(0, '/');
-session_start();
-$_SESSION['test_var'] = 'Hello World';
-$_SESSION['time'] = time();
-session_write_close();
-echo "Session set. ID: " . session_id();
-?>
